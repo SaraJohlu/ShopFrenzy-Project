d8ba16e2fc7850ee7c540b3a25c4193fbc434cf1
Sara <sara_lunden1@hotmail.com>
2025-05-02 10:09:42 +0200

    Adjust fixes

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+describe('template spec', () => {
+  // eslint-disable-next-line no-undef
+  it('passes', () => {
+    // eslint-disable-next-line no-undef
+    cy.visit('https://SaraJohlu.github.io/ShopFrenzy-Project')
+  })
+})
